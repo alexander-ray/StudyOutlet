8002e392118a111c83268f56cb2e1e38e3daf496
Alex Ray <alexray@Alexs-MacBook-Pro.local>
2017-04-01 18:11:45 -0600

    Added mysql

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,6 +1,10 @@
 import Vapor
+import VaporMySQL
 
 let drop = Droplet()
+drop.preparations.append(Question.self)
+
+try drop.addProvider(VaporMySQL.Provider.self)
 
 drop.get("hello") { request in
     let name = request.data["name"]?.string ?? "stranger"
@@ -8,6 +12,12 @@ drop.get("hello") { request in
     return try drop.view.make("hello", [
         "name":name
     ])
+}
+
+drop.post("question") { req in
+    var question = try Question(node: req.json)
+    try question.save()
+    return try question.makeJSON()
 }
 
 drop.run()
