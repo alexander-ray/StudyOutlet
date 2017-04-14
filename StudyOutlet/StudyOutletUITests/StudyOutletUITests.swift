14834752ce122816d1186d952d28a1a84850c2f2
Alex Ray <1alexray@gmail.com>
2017-04-14 15:46:39 -0600

    Added login tests

--- StudyOutlet/StudyOutletUITests/StudyOutletUITests.swift
+++ StudyOutlet/StudyOutletUITests/StudyOutletUITests.swift
@@ -9,7 +9,8 @@
 import XCTest
 
 class StudyOutletUITests: XCTestCase {
-        
+    var app: XCUIApplication!
+
     override func setUp() {
         super.setUp()
         
@@ -18,8 +19,8 @@ class StudyOutletUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app = XCUIApplication()
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +29,65 @@ class StudyOutletUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Basic "good" input for a login
+    // Should continue to main menu
+    func testLoginFieldPositive1() {
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        let loginButton = app.buttons["LoginButton"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("alex@alex.com")
+        passwordTextField.tap()
+        passwordTextField.typeText("1Alex")
+        
+        loginButton.tap()
+        XCTAssert(app.images["GRE_Title.png"].exists)
     }
-    
+    // Another "good" input
+    func testLoginFieldPositive2() {
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        let loginButton = app.buttons["LoginButton"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("a.nonymous@example.com")
+        passwordTextField.tap()
+        passwordTextField.typeText("1Alex")
+        
+        loginButton.tap()
+        XCTAssert(app.images["GRE_Title.png"].exists)
+    }
+    // Another "good" input
+    func testLoginFieldPositive3() {
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        let loginButton = app.buttons["LoginButton"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("name+tag@example.com")
+        passwordTextField.tap()
+        passwordTextField.typeText("1Alex")
+        
+        loginButton.tap()
+        XCTAssert(app.images["GRE_Title.png"].exists)
+    }
+
+
+    // Empty input fields
+    // Should not go to Main Menu
+    func testLoginFieldNegative1() {
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        let loginButton = app.buttons["LoginButton"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("")
+        passwordTextField.tap()
+        passwordTextField.typeText("")
+        
+        loginButton.tap()
+        XCTAssert(!app.images["GRE_Title.png"].exists)
+    }
+
 }
