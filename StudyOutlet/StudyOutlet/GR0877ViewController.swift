9ade291bcf85ccccb392d37b0fba3001d4d0127a
Alex Ray <1alexray@gmail.com>
2017-04-17 15:49:11 -0600

    Added comments

--- StudyOutlet/StudyOutlet/GR0877ViewController.swift
+++ StudyOutlet/StudyOutlet/GR0877ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    // Get number of minutes and questions from user defaults
     var minutes = defaults.integer(forKey: "test_length")
     var numQuestions = defaults.integer(forKey: "num_questions")
 
+    // Declarations
     var timer = Timer()
     var test = Test(testName: "GR0877")
     var questionIndex = 0
     var currentAnswer = ""
     
+    // Answers for answerPicker
     let answers = ["A", "B", "C", "D", "E"]
     
     // MARK: Outlets
@@ -41,8 +44,10 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     // MARK: Actions
     @IBAction func StartAction(_ sender: Any)
     {
+        // Start timer using "counter" function
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(GR0877ViewController.counter), userInfo: nil, repeats: true)
         
+        // Setting up inputs
         StartImageOutlet.isHidden = true
         StartButtonOutlet.isHidden = true
         StopImageOutlet.isHidden = false
@@ -56,14 +61,17 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     
     @IBAction func StopAction(_ sender: Any)
     {
+        // Invalidate timer
         timer.invalidate()
-        minutes = 170
-
+        
+        // Send "are you sure?" alert
         let alertController = UIAlertController(title: "", message: "Are you sure you want to stop this test? Your progress will not be saved.", preferredStyle: UIAlertControllerStyle.alert)
+        // If stop, return to menu
         let stopAction = UIAlertAction(title: "Stop", style: UIAlertActionStyle.default) {
             (result : UIAlertAction) -> Void in
             self.performSegue(withIdentifier: "BackToOPEMenu", sender: self)
         }
+        // If back, return to test
         let backAction = UIAlertAction(title: "Back to Test", style: UIAlertActionStyle.default) {
             (result : UIAlertAction) -> Void in
         }
@@ -73,20 +81,25 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     }
     
     @IBAction func submit(_ sender: UIButton) {
+        // If correct
         if (currentAnswer == test.questionArray[questionIndex].answer) {
+            // If final questions
             if (questionIndex == numQuestions - 1) {
                 presentFinalAlert()
             }
+            // If not final question
             else {
                 presentCorrectAlert()
             }
         }
+        // If not correct
         else {
             presentIncorrectAlert()
         }
     }
 
     // MARK: Picker
+    // Setup to conform to picker data source and picker delegate
     // returns the number of 'columns' to display.
     func numberOfComponents(in: UIPickerView) -> Int{
         return 1
@@ -106,9 +119,10 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         currentAnswer = answers[row]
     }
 
+    // Initially loading view
     override func viewDidLoad() {
         super.viewDidLoad()
-        // If there was no data in user defaults
+        // If there was no data in user defaults, set to reasonable numbers
         if (minutes == 0) {
             minutes = 170
         }
@@ -116,20 +130,25 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
             numQuestions = 6
         }
         
+        // Set minutes counter
         countMinute.text = String(minutes) + " min"
         
+        // Update days until next test countdown
         let date = (defaults.object(forKey: "next_test_date") ?? Date()) as! Date
         let days = Helper.numDaysBeforeTest(testDate: date)
         CountDown.text = String(days) + " Days Until next Test"
         
+        // Get questions from test model
         test.getQuestions() { arr in
             self.test.questionArray = arr!
+            // Set first question image if possible
             if (self.numQuestions > 0) {
                 let question = self.test.questionArray[0].question
                 self.imageView.image = question
             }
         }
         
+        // Button and input setup
         imageOutlet.isHidden = true
         rightImageOutlet.isHidden = true
         nextOutlet.isHidden = true
@@ -144,7 +163,7 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         answerPicker.dataSource = self        
     }
 
-    
+    // Counter function for timer
     func counter()
     {
         minutes -= 1
@@ -152,6 +171,7 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         
         if (minutes == 0)
         {
+            // If ran out of time, alert
             timer.invalidate()
             let alertController = UIAlertController(title: "", message: "You ran out of time!", preferredStyle: UIAlertControllerStyle.alert)
             let returnAction = UIAlertAction(title: "Return to menu", style: UIAlertActionStyle.default) {
@@ -163,6 +183,7 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         }
     }
     
+    // Increment question index, update image
     func nextQuestion() {
         if (questionIndex < numQuestions)
         {
@@ -172,17 +193,19 @@ class GR0877ViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         }
 
     }
-    func previousQuestion() {
+    
+    // Unused at the moment
+    /*func previousQuestion() {
         if (questionIndex > 0)
         {
             questionIndex = questionIndex - 1
             let question = self.test.questionArray[self.questionIndex].question
             self.imageView.image = question
         }
-    }
+    }*/
     
     
-    // Alerts
+    // Various alert setup
     func presentCorrectAlert() {
         // Set up "invalid date" alert
         let alertController = UIAlertController(title: "Correct!", message: "", preferredStyle: UIAlertControllerStyle.alert)
