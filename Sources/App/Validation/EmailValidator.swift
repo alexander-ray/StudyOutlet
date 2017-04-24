ea58db8302b10d4a18ac5f6297a5cde42eb663e3
woosung <woja6494@colorado.edu>
2017-04-24 14:24:57 -0600

    add comment

--- Sources/App/Validation/EmailValidator.swift
+++ Sources/App/Validation/EmailValidator.swift
@@ -3,7 +3,7 @@ import Vapor
 
 class EmailValidator: ValidationSuite {
     static func validate(input value: String) throws {
-        let range = value.range(of: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: .regularExpression)
+        let range = value.range(of: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: .regularExpression) //set valid email range with regular expression
         guard let _ = range else {
             throw error(with: value)
         }
--- Sources/App/Validation/PasswordValidator.swift
+++ Sources/App/Validation/PasswordValidator.swift
@@ -3,8 +3,7 @@ import Vapor
 
 class PasswordValidator: ValidationSuite {
   static func validate(input value: String) throws {
-    let range = value.range(of: "^(?=.*[0-9])(?=.*[A-Z])", options: .regularExpression)
-    guard let _ = range else {
+    let range = value.range(of: "^(?=.*[0-9])(?=.*[A-Z])", options: .regularExpression) //set valid password range with regular expression
       throw error(with: value)
     }
   }
